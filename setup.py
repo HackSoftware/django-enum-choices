f354291c2f2f098a716c3ff90417c2ec24edbfd6
Vasil Slavov <v.slavov96@gmail.com>
2019-07-04 11:49:11 +0300

    Bump Python requirement. Add Django classifiers

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ DESCRIPTION = "A custom Django field able to use subclasses of Python's internal
 URL = 'https://github.com/HackSoftware/django-enum-choices'
 EMAIL = 'vasil.slavov@hacksoft.io'
 AUTHOR = 'Vasil Slavov'
-REQUIRES_PYTHON = '>=3.6.0'
+REQUIRES_PYTHON = '>=3.5.0'
 VERSION = None
 
 # What packages are required for this module to be executed?
@@ -111,8 +111,18 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: Implementation :: CPython'
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Framework :: Django',
+        'Framework :: Django :: 1.10',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.1',
+        'Framework :: Django :: 2.2',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries'
     ],
     # $ setup.py publish support.
     cmdclass={

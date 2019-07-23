1c0cc049f8e4056455e314154a8717bce0b34c71
Vasil Slavov <v.slavov96@gmail.com>
2019-07-23 14:36:10 +0300

    Add initial form field implementation

--- django_enum_choices/utils.py
+++ django_enum_choices/utils.py
@@ -1,3 +1,12 @@
+from typing import Callable, Tuple
+from enum import Enum
+from itertools import chain
+
+from django.utils.translation import gettext as _
+
+from .exceptions import EnumChoiceFieldException
+
+
 def as_choice_builder(choice_builder):
     def inner(enumeration):
         if not enumeration:
@@ -15,3 +24,25 @@ def value_from_built_choice(built_choice):
         return built_choice[0]
 
     return built_choice
+
+
+def build_enum_choices(
+    enum_class: Enum,
+    choice_builder: Callable
+) -> Tuple[Tuple[str]]:
+    choices = [
+        choice_builder(choice)
+        for choice in enum_class
+    ]
+
+    if not all(
+        isinstance(value, str) for value
+        in chain.from_iterable(choices)
+    ):
+        raise EnumChoiceFieldException(
+            _('All choices generated from {} must be strings.'.format(
+                enum_class.__name__
+            ))
+        )
+
+    return choices

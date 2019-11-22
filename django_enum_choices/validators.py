be0e50da20869f68b07beeb035245d6aee95365f
wencakisa <wencakisa@gmail.com>
2019-11-22 14:32:42 +0200

    Providing validate_attribute_name to EnumValueMaxLengthValidator
    * If this boolean is provided -> the Enum field name should be validated
    in clean() instead of the value

--- django_enum_choices/validators.py
+++ django_enum_choices/validators.py
@@ -7,6 +7,11 @@ class EnumValueMaxLengthValidator(MaxLengthValidator):
     attempts to return `len(value)` when value is an enumeration
     instance, which raises an error
     """
+    def __init__(self, validate_attribute_name: bool = False, *args, **kwargs):
+        self.validate_attribute_name = validate_attribute_name
+
+        super().__init__(*args, **kwargs)
 
     def clean(self, x):
-        return len(str(x.value))
+        value_to_validate = x.name if self.validate_attribute_name else x.value
+        return len(str(value_to_validate))
